d3af10f76fb2f4f6e968a28d59e7ac24b569d730
Service Web <sw@gssmail.com>
2024-03-11 08:53:03 -0500

    Commit changes in C:\Websites\Git\emarlow\3906\GCG_3906_WC_WklyLoad.sql

---
+++ GCG_3906_WC_WklyLoad.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "GCG_3906_WC_WklyLoad"(
+ "Terminal" CHAR(3),
+ "Workcenter" CHAR(4),
+ "Week_No" INTEGER,
+ "Date_Start" DATE,
+ "Date_end" DATE,
+ "Total_Schedule" DOUBLE,
+ "Total_Capacity" DOUBLE);
